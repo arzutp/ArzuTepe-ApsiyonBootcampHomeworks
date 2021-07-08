3d06a6f21f2a5e9461f62a21dbdad0f6c9367df5
arzutepe <38106558+arzutp@users.noreply.github.com>
2021-07-08 21:32:29 +0300

    Add files via upload

---
+++ SmsService/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string token;
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (var response = await client.GetAsync("http://localhost:60178/api/auth/login?userName=n&password=a"))
+                {
+                    token= await response.Content.ReadAsStringAsync();
+
+                    token = "dsdsdsds";
+
+                }
+
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+                using (var response = await client.GetAsync("http://localhost:60178/api/home/index"))
+                {
+
+                    string result = await response.Content.ReadAsStringAsync();
+
+
+                }
+
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SmsService/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(string userName, string email, string company)
+        {
+            HttpClient client = new HttpClient();
+            string token;
+
+            var response = await client.GetAsync("http://localhost:60178/api/auth/login?userName=a&password=a");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                token = await response.Content.ReadAsStringAsync();
+
+
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+                string serialized=  JsonSerializer.Serialize(new { userName = userName, email = email, name = userName, company = company });
+
+                StringContent content = new StringContent(serialized, Encoding.UTF8, "application/json");
+
+                
+                await client.PostAsync("http://localhost:60178/api/email/adduser", content);
+
+            }
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                string error = response.Headers.GetValues("WwwAuthorize").FirstOrDefault();
+            }
+
+
+
+
+            return View();
+        }
+    }
+}
