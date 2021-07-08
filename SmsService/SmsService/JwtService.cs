f0ba992d92953403a06fd8435f440f4bd02d708d
arzutepe <38106558+arzutp@users.noreply.github.com>
2021-07-08 21:34:20 +0300

    Add files via upload

---
+++ SmsService/SmsService/JwtService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SmsService.Data;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsService
+{
+    public class JwtService
+    {
+        private IConfiguration _configuration;
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateAccessToken(User user)
+        {
+            //        string signingKey = _configuration.GetValue<string>("Jwt:Key");
+            //        string issuer = _configuration.GetValue<string>("Jwt:Issuer");
+            //        int hours = _configuration.GetValue<int>("Jwt:HoursValid");
+            //        System.DateTime expireDateTime = System.DateTime.UtcNow.AddHours(hours);
+
+            //        byte[] signingKeyBytes = System.Text.Encoding.UTF8.GetBytes(signingKey);
+            //        SymmetricSecurityKey secKey = new SymmetricSecurityKey(signingKeyBytes);
+            //        SigningCredentials creds = new SigningCredentials(secKey, SecurityAlgorithms.HmacSha256);
+
+            //        var authClaims = new List<Claim>
+            //{
+            //    new Claim(ClaimTypes.Name, user.Name)
+            //};
+
+            //        JwtSecurityToken token = new JwtSecurityToken(
+            //            issuer: issuer,
+            //            audience: issuer,
+            //            claims: authClaims,
+            //            expires: System.DateTime.UtcNow.AddHours(hours),
+            //            signingCredentials: creds
+            //        );
+            //        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            //        string writtenToken = handler.WriteToken(token);
+
+            //        return writtenToken;
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"] , signingCredentials: signingCredentials,
+                expires:DateTime.Now.AddHours(1));
+
+            //SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor
+            //{
+            //    Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] {
+            //          new Claim(ClaimTypes.Name,user.Name),
+            //          new Claim(ClaimTypes.Email, user.Email)
+            //    }),
+            //    SigningCredentials = signingCredentials
+            //};
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            //jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            return jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+        }
+    }
+}
---
+++ SmsService/SmsService/Startup.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using SmsService.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SmsDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<SmsDbContext>();
+
+            services.AddCors();
+            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+
+
+                };
+            });
+
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmsService", Version = "v1" });
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmsService v1"));
+            }
+
+            app.UseRouting();
+
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
